29289e1d64414c0c325c698afe6a2a385c02d1c5
Dell <browski999@gmail.com>
2019-07-01 22:15:04 +0100

    Some more work

---
+++ NetLore.Infrastructure/Extensions/AutoMapperRegistrationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NetLore.Infrastructure.Extensions
+{
+    public static class AutoMapperRegistrationExtensions
+    {
+        public static IServiceCollection AddAutoMapperWithProfiles(this IServiceCollection services, Assembly assembly)
+        {
+            services.AddAutoMapperWithProfiles(Assembly.GetAssembly(typeof(AutoMapperRegistrationExtensions)));
+            return services;
+        }
+
+    }
+}
---
+++ NetLore.Infrastructure/Profiles/TaskProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+
+namespace NetLore.Infrastructure.Profiles
+{
+    public class TaskProfile : Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<Domain.Entities.Task, Domain.Models.Task>()
+                .ForMember(target => target.Id, source => source.MapFrom(prop => prop.Id))
+                .ForMember(target => target.Name, source => source.MapFrom(prop => prop.Name))
+                .ForMember(target => target.Description, source => source.MapFrom(prop => prop.Description))
+                .ForMember(target => target.IsCompleted, source => source.MapFrom(prop => prop.IsCompleted))
+                .ForSourceMember(source => source.TaskList, option => option.DoNotValidate())
+                .ForSourceMember(source => source.TaskListId, option => option.DoNotValidate());
+
+            CreateMap<Domain.Models.Task, Domain.Entities.Task>()
+                .ForMember(target => target.Id, source => source.MapFrom(prop => prop.Id))
+                .ForMember(target => target.Name, source => source.MapFrom(prop => prop.Name))
+                .ForMember(target => target.Description, source => source.MapFrom(prop => prop.Description))
+                .ForMember(target => target.IsCompleted, source => source.MapFrom(prop => prop.IsCompleted))
+                .ForMember(source => source.TaskList, option => option.Ignore())
+                .ForMember(source => source.TaskListId, option => option.Ignore());
+        }
+    }
+}
